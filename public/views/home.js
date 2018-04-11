a2c7d4e37c8dc68cbd3c5ee83aca7cdbc65a83b4
trung.tt <trung@kvytechnology.com>
2018-04-11 17:00:35 +0700

    Props and State

--- public/views/home.js
+++ public/views/home.js
@@ -1,14 +1,45 @@
-var Title = (props) => {
-  return <h1>Hello, {props.children} !</h1>
+class Title extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, {this.props.children} !</h1>
+      </div>
+    )
+  }
 }
 
-var Welcome = (props) => {
-  return (
-    <h2> Welcome {props.name} </h2>
-  )
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {counter: 0};
+  }
+  clickToMe() {
+    alert(this.props.name)
+  }
+  increase() {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1
+    }))
+  }
+  render() {
+    return (
+      <div>
+        <h2> Welcome {this.props.name} </h2>
+        <h2> {this.state.counter} </h2>
+        <button onClick={() => this.clickToMe()}>Click To Me</button>
+        <button onClick={() => {getName(this.props.name)}}>Click To Me</button>
+        <button onClick={() => this.increase()}>count</button>
+      </div>
+    )
+  }
+
 }
 
-var _render = () => {
+var getName = (name) => {
+  alert(name)
+}
+
+var _element = () => {
   const _Element =
   <div>
     <Title>ReactJS</Title>
@@ -19,4 +50,5 @@ var _render = () => {
     _Element
     , document.getElementById('root'))
 }
-setInterval(_render, 1000)
+
+setInterval(_element, 1000)
