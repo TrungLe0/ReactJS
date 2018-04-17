ae5c0e2c1a8419a685517736235c2d758cab72f7
trung.tt <trung@kvytechnology.com>
2018-04-17 10:35:44 +0700

    use local store - not yet

--- public/views/home.js
+++ public/views/home.js
@@ -5,28 +5,32 @@ function Title() {
 class InputTag extends React.Component {
   constructor(props) {
     super(props)
-    this.myText = React.createRef()
-    this.state = {value: ''}
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+  }
+
+  setResult(value) {
+    this.setState({
+      value: value
+    })
+  }
+
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.props.todoChange(this.state.value)
+    }
   }
 
   render() {
     return (
       <div>
-        <input type="text" value={this.state.value} />
-        <Button />
+        <input type="text" onChange={(e) => this.setResult(e.target.value)} onKeyPress={this.handleKeyPress}/>
+        <div className="btn-group">
+          <button className="btn-clear" >Clear</button>
+          <button onClick={(e) => this.props.todoChange(this.state.value)} className="btn-add">Add</button>
+        </div>
       </div>
     )
   }
-}
-//
-
-function Button() {
-  return (
-    <div className="btn-group">
-      <button className="btn-clear">Clear</button>
-      <button className="btn-add">Add</button>
-    </div>
-  )
 }
 
 function Todos(props) {
@@ -34,31 +38,59 @@ function Todos(props) {
     <ul>
       {
         props.result.map((todo) => {
-          return <Todo key={ todo['id'] } todo={ todo } />
+          return <Todo key={todo['id']} todo={todo} remove={props.remove} checkDone={props.checkDone} />
         })
       }
     </ul>
   )
 }
 
-function Todo(props) {
-  let classCheck = ['fa fa-check']
-  if (props.todo['done'] === true)
-    classCheck = classCheck.concat('checked').join(' ')
-  return (
-    <li>
-      <span className={ classCheck }></span>
-      {props.todo['name']}
-      <span className="fa fa-trash-o btn-remove"></span>
-    </li>
-  )
+class Todo extends React.Component {
+  constructor(props, state) {
+    super(props, state)
+    console.log(this.state)
+    this.removeTodo = this.removeTodo.bind(this)
+    this.checkDone = this.checkDone.bind(this)
+  }
+
+  removeTodo(id) {
+    console.log(this.state)
+    let newResult = this.state.result.filter(result => result['id'] !== id )
+    this.setState({result: newResult})
+    localStorage.removeItem('todo' + id)
+  }
+
+  checkDone(id, e) {
+    if (e.target.className.indexOf('btn-remove') < 0) {
+      for (var i = 0; i < this.state.result.length; i++) {
+        let item = this.state.result[i]
+        if (item['id'] === id){
+          item['done'] = !item['done']
+          localStorage.setItem('todo' + id, JSON.stringify(item))
+        }
+      }
+    }
+  }
+  render() {
+    let classCheck = ['fa fa-check']
+    if (this.props.todo['done'] === true)
+      classCheck = classCheck.concat('checked').join(' ')
+    return (
+      <li onClick={(e) => this.checkDone(this.props.todo['id'])}>
+        <span className={ classCheck }></span>
+        {this.props.todo['name']}
+        <span onClick={ (e) => this.removeTodo(this.props.todo['id'], e)} className="fa fa-trash-o btn-remove"></span>
+      </li>
+    )
+  }
+
 }
 
-function CreateForm() {
+function CreateForm(props) {
   return (
     <div className="form">
       <Title />
-      <InputTag />
+      <InputTag todoChange={props.onAddData}/>
     </div>
   )
 }
@@ -66,35 +98,64 @@ function CreateForm() {
 class CreateResult extends React.Component {
   constructor(props) {
     super(props)
-    this.state ={result: [
-      {
-        id: 1,
-        name: 'AngularJS',
-        done: false
-      },
-      {
-        id: 2,
-        name: 'NodeJS',
-        done: true
-      }
-    ]}
   }
+
   render() {
     return (
       <div className="result">
-        <Todos result={this.state.result} />
+        <Todos result={this.props.result} remove={this.props.remove} checkDone={this.props.checkDone} />
       </div>
     )
   }
 
 }
 
+if (!localStorage.getItem('storeId')) localStorage.setItem('storeId', 1)
+
 class Container extends React.Component {
+  constructor(props) {
+    super(props)
+    this.addTodo = this.addTodo.bind(this)
+
+    var todos = []
+    if(localStorage) {
+      for (var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i)
+        if (key.substring(0, 4) === 'todo') {
+          var item = JSON.parse(localStorage.getItem(key))
+          todos.push(item)
+        }
+      }
+      if (todos.length === 0) {
+        let angular = {
+          id: 1,
+          name: 'AngularJS',
+          done: false
+        }
+        todos.push(angular)
+        let key = 'todo' + localStorage.getItem('storeId')
+        localStorage.setItem(key, JSON.stringify(angular))
+      }
+    }
+    this.state = {result: todos}
+  }
+
+  addTodo(name) {
+    let id = Number(localStorage.getItem('storeId')) + 1
+    let key = 'todo' + id
+    let todo = {id: id, name: name, done: false}
+    localStorage.setItem('storeId', id)
+    localStorage.setItem(key, JSON.stringify(todo))
+    this.setState({
+      result: this.state.result.concat(todo)
+    })
+  }
+
   render() {
     return (
       <div className="container">
-        <CreateForm />
-        <CreateResult />
+        <CreateForm onAddData={this.addTodo}/>
+        <CreateResult result={this.state.result} />
       </div>
     )
   }
